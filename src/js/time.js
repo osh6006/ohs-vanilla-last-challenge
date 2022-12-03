0798bbb7a0e377d328b7179791fb12b94bcda968
Hwang Seok O <ohs6006@gmail.com>
2022-12-03 21:32:39 +0900

    타이머 UI & 타이머 기능

--- src/js/time.js
+++ src/js/time.js
@@ -1,1 +1,25 @@
-class Time {}
+class Time {
+  clock;
+  constructor() {
+    this.assignElement();
+    this.addEvent();
+    this.startClock();
+  }
+  assignElement() {
+    this.clock = document.querySelector(".time-clock");
+  }
+  addEvent() {}
+
+  getTime() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    this.clock.innerText = `${hours}:${minutes}:${seconds}`;
+  }
+  startClock() {
+    setInterval(this.getTime.bind(this), 1000);
+  }
+}
+
+new Time();
